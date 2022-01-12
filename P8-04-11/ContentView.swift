88397924f0d6e6700af26c065144de9bc344bee1
Vahtee Boo <vahteeboo@gmail.com>
2022-01-12 11:21:50 +0200

    Done

--- P8-04-11/ContentView.swift
+++ P8-04-11/ContentView.swift
@@ -7,10 +7,35 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Decode JSON") {
+            let input = """
+            {
+            "name": "Taylor Swift",
+            "address": {
+                    "street": "555, Taylor Swift Avenue",
+                    "city": "Nashville"
+                }
+            }
+            """
+            
+            let data = Data(input.utf8)
+            
+            if let user = try? JSONDecoder().decode(User.self, from: data) {
+                print(user.address.street)
+            }
+        }
     }
 }
 
